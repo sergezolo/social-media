69b0817dc6a1088eb74efd9ef3501a56b28e18ef
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2021-08-31 18:00:10 -0500

    Add express to firebase functions

--- functions/index.js
+++ functions/index.js
@@ -3,45 +3,48 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+const express = require('express');
+const app = express();
 
-exports.getScreams = functions.https.onRequest((request, response) => {
+app.get('/screams', (request, response) => {
     admin
-    .firestore()
-    .collection('screams')
-    .get()
-    .then(data => {
-        let screams = [];
-        data.forEach(doc => {
-            screams.push(doc.data());
+        .firestore()
+        .collection('screams')
+        .orderBy('createdAt', 'desc')
+        .get()
+        .then((data) => {
+            let screams = [];
+            data.forEach((doc) => {
+                screams.push({
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt,
+                });
+            });
+            return response.json(screams);
         })
-        return response.json(screams);
-    })
-    .catch(error => console.error(error));
+        .catch((err) => console.error(err));
 });
 
-exports.createScream = functions.https.onRequest((request, response) => {
-    if(request.method !== 'POST'){
-        return response.status(400).json({ error: 'Method not allowed!' });
-    }
+app.post('/scream', (request, response) => {
     const newScream = {
         body: request.body.body,
         userHandle: request.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+        createdAt: new Date().toISOString(),
     };
 
     admin
         .firestore()
         .collection('screams')
         .add(newScream)
-        .then(document => {
-            response.json({ message: `Document ${document.id} created successfully!` });
+        .then((doc) => {
+            response.json({ message: `Document ${doc.id} created successfully!` });
         })
-        .catch(error => {
+        .catch((err) => {
             response.status(500).json({ error: 'Oops! Something went wrong.' });
-            console.error(error);
+            console.error(err);
         });
 });
+
+exports.api = functions.https.onRequest(app);
