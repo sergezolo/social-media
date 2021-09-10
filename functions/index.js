04fff4371dcdea7bb458db93fbf896cebada4f74
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2021-09-09 20:31:46 -0500

    Fix bugs, add signup

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,20 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
+const app = require('express')();
 
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const firebaseConfig = {
+  apiKey: "AIzaSyA99gFmpIjWdDZRw7Nzc1etSy_97BNGrso",
+  authDomain: "social-media-89aaf.firebaseapp.com",
+  projectId: "social-media-89aaf",
+  storageBucket: "social-media-89aaf.appspot.com",
+  messagingSenderId: "354713403964",
+  appId: "1:354713403964:web:de08cd032d7660f973b939"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
 
 app.get('/screams', (request, response) => {
     admin
@@ -20,31 +30,58 @@ app.get('/screams', (request, response) => {
                     body: doc.data().body,
                     userHandle: doc.data().userHandle,
                     createdAt: doc.data().createdAt,
-                });
-            });
+                })
+            })
             return response.json(screams);
         })
-        .catch((err) => console.error(err));
-});
+        .catch((err) => {
+            response.status(500).json({ error: err });
+            console.error(err);
+        });
+})
 
 app.post('/scream', (request, response) => {
     const newScream = {
         body: request.body.body,
         userHandle: request.body.userHandle,
         createdAt: new Date().toISOString(),
-    };
-
+    }
     admin
         .firestore()
         .collection('screams')
         .add(newScream)
-        .then((doc) => {
-            response.json({ message: `Document ${doc.id} created successfully!` });
+        .then ((doc) => {
+            response.status(201).json({ message: `Document ${doc.id} created successfully!` });
         })
         .catch((err) => {
             response.status(500).json({ error: 'Oops! Something went wrong.' });
             console.error(err);
         });
+})
+
+//SignUp
+app.post('/signup', (request, response) => {
+    const newUser = {
+        email: request.body.email,
+        password: request.body.password,
+        confirmPassword: request.body.confirmPassword,
+        handle: request.body.handle,
+    };
+
+    // TODO: validate data
+
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(newUser.email, newUser.password)
+        .then((data) => {
+            response.status(201).json({ message: `User ${data.user.uid} signed up successfully` });
+        })
+        .catch((err) => {
+            response.status(500).json({ error: err.code });
+            console.error(err);
+        });
 });
 
-exports.api = functions.https.onRequest(app);
+
+// https://baseurl.com/api/...
+exports.api = functions.https.onRequest(app)
