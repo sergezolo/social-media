a59f11fa6d021c21dca480f45c7e732d06dd0a7a
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2021-09-12 00:24:36 -0500

    Add login and validate credentials

--- functions/index.js
+++ functions/index.js
@@ -132,6 +132,38 @@ app.post('/signup', (request, response) => {
         });
 });
 
+//LogIn
+app.post('/login', (request, response) => {
+    const user = {
+        email: request.body.email, 
+        password: request.body.password
+    };
+
+    let errors = {};
+    
+    if(isEmpty(user.email)) { errors.email = "Must not be empty" } 
+    if (isEmpty(user.password)) { errors.password = "Must not be empty" }
+
+    if (Object.keys(errors).length > 0) return response.status(400).json(errors);
+
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(user.email, user.password)
+        .then((data) => { 
+            return data.user.getIdToken();
+        })
+        .then((token) => {
+            return response.json({ token });
+        })
+        .catch((err) => {
+            console.error(err);
+            if (err.code !== '') {
+                return response.status(403).json({ email: 'Wrong credentials, please try again' });
+            } else {
+                return response.status(500).json({ error: err.code })
+            }
+        })
+});
 
 // https://baseurl.com/api/...
 exports.api = functions.https.onRequest(app)
