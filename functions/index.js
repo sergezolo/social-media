0abb939553639453d1ea20ce98cc3ffff73a44ff
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2021-09-11 23:47:51 -0500

    Validate user info on signing up

--- functions/index.js
+++ functions/index.js
@@ -59,6 +59,18 @@ app.post('/scream', (request, response) => {
         });
 })
 
+//Helper function for validation 
+const isEmpty = (string) => {
+    if (string.trim() === '') return true;
+    else return false;
+}
+
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; 
+    if (email.match(regEx)) return true;
+    else return false;
+}
+
 //SignUp
 app.post('/signup', (request, response) => {
     const newUser = {
@@ -67,11 +79,20 @@ app.post('/signup', (request, response) => {
         confirmPassword: request.body.confirmPassword,
         handle: request.body.handle,
     };
+    
+    // !!!User credentials validations
+    let errors = {};
+    
+    if(isEmpty(newUser.email)) { errors.email = "Must not be empty" } 
+    else if (!isEmail(newUser.email)) { errors.email = "Must be a valid email address" }
+    if (isEmpty(newUser.password)) { errors.password = "Must not be empty" }
+    if (newUser.password !== newUser.confirmPassword) { errors.password = "Passwords don't match" }
+    if (isEmpty(newUser.handle)) { errors.handle = "Must not be empty" }
+
+    if (Object.keys(errors).length > 0) return response.status(400).json(errors);
 
     // TODO: validate data
-
     let token, userId;
-
     db
         .doc( `/users/${newUser.handle}`)
         .get()
@@ -101,14 +122,6 @@ app.post('/signup', (request, response) => {
         .then(() => {
             response.status(201).json({ token })
         })
-
-
-    // firebase
-    //     .auth()
-    //     .createUserWithEmailAndPassword(newUser.email, newUser.password)
-    //     .then((data) => {
-    //         response.status(201).json({ message: `User ${data.user.uid} signed up successfully` });
-    //     })
         .catch((err) => {
             if (err.code === "auth/email-already-in-use") {
                 return response.status(400).json({ email: 'This email is already in use' });
